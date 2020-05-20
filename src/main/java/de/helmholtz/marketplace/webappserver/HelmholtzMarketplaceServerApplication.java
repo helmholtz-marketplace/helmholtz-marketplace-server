8dc41ddb3797581ab52c3784ab65b998c64d2b0c
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2020-05-20 21:37:00 +0200

    helmholtz-marketplace-server: first commit
    Motivation:
    
    A web server for helmholtz marketplace. This will be use to serve
    the static files in `helmholtz-marketplace-webapp` repository.
    
    Modification:Ã
    
    Add boilerplate code generated by Spring Initializr.
    
    Result:
    
    A spring boot application that will be use to serve web related
    static files and oauth2 client.

---
+++ src/main/java/de/helmholtz/marketplace/webappserver/HelmholtzMarketplaceServerApplication.java
@@ -0,0 +1,12 @@
+package de.helmholtz.marketplace.webappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelmholtzMarketplaceServerApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(HelmholtzMarketplaceServerApplication.class, args);
+	}
+}
---
+++ src/test/java/de/helmholtz/marketplace/webappserver/HelmholtzMarketplaceServerApplicationTests.java
@@ -0,0 +1,12 @@
+package de.helmholtz.marketplace.webappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelmholtzMarketplaceServerApplicationTests
+{
+	@Test
+	void contextLoads() {
+	}
+}
